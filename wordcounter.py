7081a37fe08d6c73f83c5bd66ceccaad5eb46a16
stamarty <7009764+stamarty@users.noreply.github.com>
2019-09-05 12:36:24 -0700

    Testing commit from PyCharm

--- wordcounter.py
+++ wordcounter.py
@@ -1,12 +1,13 @@
-
+#This should commit a change from here.
 #Change these to inputs values for final.
 f = open("C:/Users/Steven.Martinez/source/repos/WordCounter/KeywordSearching.txt")
 
 ## trying to split each line into its individual words and create a full list of words used.
 lines = dict()
-for line in lines:
-    words = line.split()
-    for word in words:
+
+#for line in lines:
+#    words = line.split()
+#    for word in words:
 
 masterDoc = open("C:/Users/Steven.Martinez/source/repos/WordCounter/masterdocs/master.txt", "w")
 masterDoc.write("yay!")
