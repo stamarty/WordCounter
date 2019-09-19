883d6e5ddd8d3a09d778485b0667b69b3e842ed4
doomedcow <Gymlet@users.noreply.github.com>
2019-09-19 14:40:45 -0700

    Testing commit from PyCharm

--- wordcounter.py
+++ wordcounter.py
@@ -1,19 +1,26 @@
-#trying to split each line into its individual words and create a full list of words used.
-#Make the list
+# inFile = input("Where is the file you'd like to analyze?")
+# outFile = input('Which file do you want to append to?')
 
-#inFile = input("Where is the file you'd like to analyze?")
-#outFile = input('Which file do you want to append to?')
+# Copies keywordsearching to master. Allows for manipulation while retaining original file
 
 inFile = open("KeywordSearching.txt", 'r')
-outFile = open("E:\Program Files\Repos\WordCounter\masterdocs\master.txt", 'a')
+outFile = open("masterdocs/master.txt", 'a')
 inData = inFile.read()
 inFile.close()
 outData = outFile.write(inData.upper())
 outFile.close()
 
-keywords = open('E:\Program Files\Repos\WordCounter\masterdocs\master.txt').readlines()
-keywords.sort()
-print('Sorted list: ', keywords)
+# let's try a dictionary?
 
+d = {}
+with open('masterdocs/master.txt') as keyword:
+    for line in keyword:
+        word = line.strip('\n')
+        d[word]
+print(d)
+
+# keywords = open('masterdocs/master.txt').readlines()
+# keywords.sort()
+# print('Sorted list: ', keywords)
 
 ####Current problem? How to get rid of all the '\n'
